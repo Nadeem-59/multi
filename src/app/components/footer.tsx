cb1a19d811121aa7e4d1c0e14103d59683a84671
Nadeem-59 <nadeemmaha59@gmail.com>
2024-11-10 10:27:46 -0800

    first commit

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer className="bg-violet-800 text-white flex items-center justify-center text-center h-12">
+        <p>© 2023 Mustabshira Khalid</p>
+      </footer>
+    );
+  }
